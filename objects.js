501eab50823615ad1c6f745783ad8e3ec1134f54
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-08 07:16:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,10 @@
 var playlist = { name: "title"};
+
+function updatePlaylist(playlist, artistName, songTitle)  {
+  return Object.assign({}, playlist, { [artistName]: songTitle })
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  var playlist = {artistName: "Kanye"};
+delete playlist.artistName
+}
